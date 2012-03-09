2d2801f1ee730d59984f961ac607a33d0e6a4e3a
Johannes Alneberg <alneberg@kth.se>
2012-03-09 16:10:58 +0100

    Constructed first test case

--- test/unit/profile_test.rb
+++ test/unit/profile_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class ProfileTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "insert profile" do
+    Profile.create(:name => "first!")
+    p1=Profiles.find_by_name("first!")
+    assert(p1.name == "first!")
+    assert_nil(p1.parent_profile_id)
+  end
+  #test "whether profile was inserted" do
+  #  @p=Profiles.find_by_name("first!")    
+#assert
+#  end
 end
