439e31a9647148431a0915627cb8d0148e8456f7
Johannes Alneberg <alneberg@kth.se>
2012-03-14 15:37:41 +0100

    Changed static pages titles

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,11 +1,41 @@
 require 'spec_helper'
 
-describe "StaticPages" do
-  describe "GET /static_pages" do
-    it "works! (now write some real specs)" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get static_pages_index_path
-      response.status.should be(200)
+describe "Static pages" do
+  
+  describe "Home page" do
+    
+    it "should have the content 'Home'" do
+      visit '/static_pages/home'
+      page.should have_content('Home')
+    end
+    
+    it "should have the right title" do
+      visit '/static_pages/home'
+      page.should have_selector('title', :text => "RNRdb | Home")
+    end
+  end
+  
+  describe "Help page" do
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      page.should have_content('Help')
+    end
+    
+    it "should have the right title" do
+      visit '/static_pages/help'
+      page.should have_selector('title', :text => "RNRdb | Help")
+    end
+  end
+  describe "Contact page" do
+    
+    it "should have the content 'at'" do
+      visit '/static_pages/contact'
+      page.should have_content('at')
+    end
+
+    it "should have the right title" do
+      visit '/static_pages/contact'
+      page.should have_selector('title', :text => "RNRdb | Contact")
     end
   end
 end
