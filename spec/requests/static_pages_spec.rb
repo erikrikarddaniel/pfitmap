dd5778819c4221d14fe7c96bdbdb978b8e31d3de
Johannes Alneberg <alneberg@kth.se>
2012-03-14 15:55:19 +0100

    Added full_title helper and tests

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -9,9 +9,14 @@ describe "Static pages" do
       page.should have_content('Home')
     end
     
-    it "should have the right title" do
+    it "should not have custom title" do
       visit '/static_pages/home'
-      page.should have_selector('title', :text => "RNRdb | Home")
+      page.should_not have_selector('title', :text => "RNRdb | Home")
+    end
+    
+    it "should have base title" do
+      visit '/static_pages/home'
+      page.should have_selector('title', :text => "RNRdb")
     end
   end
   
