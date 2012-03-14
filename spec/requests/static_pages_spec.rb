df4d0ff295f717aac45ef2b8ef52d7cf08ffed8f
Johannes Alneberg <alneberg@kth.se>
2012-03-14 16:54:24 +0100

    collapsed test set, not finished

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,46 +1,28 @@
 require 'spec_helper'
 
 describe "Static pages" do
-  
-  describe "Home page" do
-    
-    it "should have the content 'Home'" do
-      visit root_path
-      page.should have_content('Home')
-    end
-    
-    it "should not have custom title" do
-      visit root_path
-      page.should_not have_selector('title', :text => "RNRdb | Home")
-    end
-    
-    it "should have base title" do
-      visit root_path
-      page.should have_selector('title', :text => "RNRdb")
-    end
-  end
-  
-  describe "Help page" do
-    it "should have the content 'Help'" do
-      visit help_path
-      page.should have_content('Help')
-    end
-    
-    it "should have the right title" do
-      visit help_path
-      page.should have_selector('title', :text => "RNRdb | Help")
-    end
-  end
-  describe "Contact page" do
-    
-    it "should have the content 'at'" do
-      visit contact_path
-      page.should have_content('at')
-    end
 
-    it "should have the right title" do
-      visit contact_path
-      page.should have_selector('title', :text => "RNRdb | Contact")
-    end
+  subject { page }
+
+  describe "Home page" do
+    before { visit root_path }
+
+    it { should have_selector('h1',    text: 'RNRdb') }
+    it { should have_selector('title', text: full_title('')) }
+    it { should_not have_selector 'title', text: '| Home' }
+  end
+
+  describe "Help page" do
+    before { visit help_path }
+
+    it { should have_selector('h1',    text: 'Help') }
+    it { should have_selector('title', text: full_title('Help')) }
+  end
+
+  describe "Contact page" do
+    before { visit contact_path }
+
+    it { should have_selector('h1',    text: 'Contact') }
+    it { should have_selector('title', text: full_title('Contact')) }
   end
 end
