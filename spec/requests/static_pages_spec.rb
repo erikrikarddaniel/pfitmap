82ede104d6f864cf365f1d4d2a7a1480fb1a7a05
Johannes Alneberg <alneberg@kth.se>
2012-03-14 16:11:30 +0100

    test cases for routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,41 +5,41 @@ describe "Static pages" do
   describe "Home page" do
     
     it "should have the content 'Home'" do
-      visit '/static_pages/home'
+      visit root_path
       page.should have_content('Home')
     end
     
     it "should not have custom title" do
-      visit '/static_pages/home'
+      visit root_path
       page.should_not have_selector('title', :text => "RNRdb | Home")
     end
     
     it "should have base title" do
-      visit '/static_pages/home'
+      visit root_path
       page.should have_selector('title', :text => "RNRdb")
     end
   end
   
   describe "Help page" do
     it "should have the content 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       page.should have_content('Help')
     end
     
     it "should have the right title" do
-      visit '/static_pages/help'
+      visit help_path
       page.should have_selector('title', :text => "RNRdb | Help")
     end
   end
   describe "Contact page" do
     
     it "should have the content 'at'" do
-      visit '/static_pages/contact'
+      visit contact_path
       page.should have_content('at')
     end
 
     it "should have the right title" do
-      visit '/static_pages/contact'
+      visit contact_path
       page.should have_selector('title', :text => "RNRdb | Contact")
     end
   end
