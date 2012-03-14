9c024b0aa0a5d5ae47366ad50ce7c92836c9d1d1
Johannes Alneberg <alneberg@kth.se>
2012-03-14 17:01:40 +0100

    Finished the test cases for new routing

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,7 +7,7 @@ describe "Static pages" do
   describe "Home page" do
     before { visit root_path }
 
-    it { should have_selector('h1',    text: 'RNRdb') }
+    it { should have_selector('h1',    text: 'Home') }
     it { should have_selector('title', text: full_title('')) }
     it { should_not have_selector 'title', text: '| Home' }
   end
