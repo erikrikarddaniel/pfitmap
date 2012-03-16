93e4e8225ff3ce417a5cf8babc28b90baa9821ee
Johannes Alneberg <alneberg@kth.se>
2012-03-16 12:53:30 +0100

    test cases for profile pages

--- spec/requests/profile_pages_spec.rb
+++ spec/requests/profile_pages_spec.rb
@@ -1,11 +1,11 @@
 require 'spec_helper'
 
-describe "ProfilePages" do
-  describe "GET /profile_pages" do
-    it "works! (now write some real specs)" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get profile_pages_index_path
-      response.status.should be(200)
-    end
+describe "Profile pages" do
+  subject { page }
+  
+  describe "profile header should contain profiles" do
+    before { visit profiles_path }
+    it { should have_selector('h1', text: 'Listing profiles')}
+    it { should have_selector('h1', text: full_title('Profiles')) }
   end
 end
