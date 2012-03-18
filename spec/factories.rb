17f0ae004f27ddfbabcc3ba67c1e381064a0c248
Johannes Alneberg <alneberg@kth.se>
2012-03-18 19:09:56 +0100

    Using factory girl for creating a new profile

---
+++ spec/factories.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :profile do
+    name               "Class 1"
+    parent_profile_id  ""
+  end
+end
--- spec/requests/profile_pages_spec.rb
+++ spec/requests/profile_pages_spec.rb
@@ -9,4 +9,34 @@ describe "Profile pages" do
     it { should have_selector('h1', text: 'Listing profiles')}
     it { should have_selector('title', text: full_title('Profiles')) }
   end
+
+  describe "Showing a profile" do
+    let(:profile) { FactoryGirl.create(:profile) }
+    before {visit profile_path(profile) }
+
+    it { should have_selector('h1', text: profile.name) }
+    it { should have_selector('title', text: profile.name) }
+  end
+  
+  describe "Creating new profile" do
+    before { visit new_profile_path }
+    
+    describe "with invalid information" do
+      it "should not create a profile" do
+        expect { click_button "Create Profile" }.not_to change(Profile, :count)
+      end
+    end
+
+    describe "with valid information" do
+        before do
+          fill_in "Name", with: "Example Profile"
+        end
+
+        it "should create a profile" do
+        expect do 
+          click_button "Create Profile"
+        end.to change(Profile, :count).by(1)
+      end
+    end
+  end
 end
