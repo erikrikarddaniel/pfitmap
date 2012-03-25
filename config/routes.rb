66dd2215285cd121cc772dced2824ff412e555b0
Johannes Alneberg <alneberg@kth.se>
2012-03-25 22:41:30 +0200

    Removed possibility to edit items

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,10 @@ Pfitmap::Application.routes.draw do
   match '/contact', to: 'static_pages#contact'
 
   resources :profiles, only: [:show, :index]
-  
   resources :results, only: [:show]
-
   resources :result_rows, only: [:show]
+  resources :result_rows_sequences, only: [:show]
+  resources :sequences, only: [:show, :index]
   #  get "home/index"
 
   # The priority is based upon order of creation:
