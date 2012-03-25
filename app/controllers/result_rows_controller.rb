367430530ec3fcc585321cd2640b4102d8bf2eb9
Johannes Alneberg <alneberg@kth.se>
2012-03-26 01:22:24 +0200

    Made sequences available as a variable

--- app/controllers/result_rows_controller.rb
+++ app/controllers/result_rows_controller.rb
@@ -3,6 +3,6 @@ class ResultRowsController < ApplicationController
     @result_row = ResultRow.find(params[:id])
     @result= Result.find(@result_row.result_id)
     @profile= Profile.find(@result.profile._id)
-    @result_row_sequences = @result_row.result_rows_sequences.paginate(page: params[:page])
+    @sequences = @result_row.sequences.paginate(page: params[:page])
   end
 end
