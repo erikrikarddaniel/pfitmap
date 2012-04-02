a522f544b2518f89a7222a9d0ec21319e3633495
Johannes Alneberg <alneberg@kth.se>
2012-04-02 10:29:47 +0200

    Added the create parsing

--- app/controllers/hmm_results_controller.rb
+++ app/controllers/hmm_results_controller.rb
@@ -41,10 +41,14 @@ class HmmResultsController < ApplicationController
   # POST /hmm_results
   # POST /hmm_results.json
   def create
-    @hmm_result = HmmResult.new(params[:hmm_result])
+    # Squirrel away the file parameter to avoid problems when creating the result object
+    file = params[:hmm_result].delete(:file)
+    
+    @hmm_result = HmmResult.new(params[:hmm_result], :executed => File.mtime(file.path))
 
     respond_to do |format|
       if @hmm_result.save
+        parse_results(@result,file) if file
         format.html { redirect_to @hmm_result, notice: 'Hmm result was successfully created.' }
         format.json { render json: @hmm_result, status: :created, location: @hmm_result }
       else
@@ -54,6 +58,51 @@ class HmmResultsController < ApplicationController
     end
   end
 
+  def parse_results(result, io)
+    Result.transaction do
+      io.read.each do |line|
+        line.chomp!
+        fields = line.split(/\s+/)
+        hmm_result_row = result.hmm_result_rows.create(
+                                                       :target_name => fields[0],
+                                                       :target_acc => ( fields[1] == '-' ? fields[0].split('|')[2..3].join(':') : fields[1] ),
+                                                       :query_name => fields[2],
+                                                       :query_acc => fields[3],
+                                                       :fullseq_evalue => fields[4].to_f,
+                                                       :fullseq_score => fields[5].to_f,
+                                                       :fullseq_bias => fields[6].to_f,
+                                                       :bestdom_evalue => fields[7].to_f,
+                                                       :bestdom_score => fields[8].to_f,
+                                                       :bestdom_bias => fields[9].to_f,
+                                                       :domnumest_exp => fields[10].to_f,
+                                                       :domnumest_reg => fields[11].to_i,
+                                                       :domnumest_clu => fields[12].to_i,
+                                                       :domnumest_ov => fields[13].to_i,
+                                                       :domnumest_env => fields[14].to_i,
+                                                       :domnumest_dom => fields[15].to_i,
+                                                       :domnumest_rep => fields[16].to_i,
+                                                       :domnumest_inc => fields[17].to_i
+                                                       )
+        all_names = "#{fields[0]} #{fields[17..-1].join(" ")}"
+        separate_entries=all_names.split(/\001/)
+        separate_entries.each do |f|
+          entry_fields=f.split("|")
+          if present_db_hit = HmmDbHit.find(:gi => entry_fields[1].to_i)
+            HmmDbHitsHmmResult.create(:db_hit_id => present_db_hit.id, :result_row_id => hmm_result_row.id )
+          else            
+            hmmdbhit = HMMDBHit.create!(
+                                        :gi => entry_fields[1].to_i,
+                                        :db => entry_fields[2],
+                                        :acc => entry_fields[3],
+                                        :desc => entry_fields[4]
+                                        )
+            HMMDBHitsHMMResult.create!(:db_hit_id => hmmdbhit.id, :result_row_id => hmm_result_row.id )
+          end
+        end
+      end
+    end
+  end
+  
   # PUT /hmm_results/1
   # PUT /hmm_results/1.json
   def update
