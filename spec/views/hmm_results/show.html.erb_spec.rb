69ce05ba3839436d528d5d44a368ceb39c0dff0b
Johannes Alneberg <alneberg@kth.se>
2012-05-08 13:53:29 +0200

    No real changes

--- spec/views/hmm_results/show.html.erb_spec.rb
+++ spec/views/hmm_results/show.html.erb_spec.rb
@@ -1,15 +1,13 @@
 # require 'spec_helper'
 
 # describe "hmm_results/show" do
-#   before(:each) do
-#     @hmm_result = assign(:hmm_result, stub_model(HmmResult,
-#       :result_db => nil
-#     ))
-#   end
+#   let!(:profile1) { FactoryGirl.create(:hmm_profile, name: "class 1" ) }
+#   let!(:sequence_db) { FactoryGirl.create(:sequence_db) }
+#   let!(:hmm_result) { FactoryGirl.create(:hmm_result, hmm_profile: profile1, sequence_db: sequence_db, executed: 100.years.ago) }
+#   let!(:result_row) { FactoryGirl.create(:hmm_result_row, hmm_result: hmm_result) }
+#   let!(:result_row2) { FactoryGirl.create(:hmm_result_row2, hmm_result: hmm_result) }
 
-#   it "renders attributes in <p>" do
-#     render
-#     # Run the generator again with the --webrat flag if you want to use webrat matchers
-#     rendered.should match(//)
+#   it "shows the result rows basic information" do
+#     page.should have_content(hmm_result_row.fullseq_evalue.to_s)
 #   end
 # end
