df83670406566b5fcfd1c007359c71bb898eae68
Johannes Alneberg <alneberg@kth.se>
2012-05-08 10:57:35 +0200

    Added something for using file fixture upload

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,6 +7,7 @@ require 'rspec/autorun'
 # Add this to load Capybara integration
 require 'capybara/rspec'
 require 'capybara/rails'
+include ActionDispatch::TestProcess
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
@@ -20,7 +21,7 @@ RSpec.configure do |config|
   # config.mock_with :mocha
   # config.mock_with :flexmock
   # config.mock_with :rr
-
+  
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
