cc4ca971f62ab8940c71107e129def500bdb20aa
Johannes Alneberg <alneberg@kth.se>
2012-05-08 16:19:48 +0200

    Unifinished work with all_hits

--- app/models/db_sequence.rb
+++ app/models/db_sequence.rb
@@ -14,4 +14,16 @@ class DbSequence < ActiveRecord::Base
   attr_accessible :hmm_result_row_id, :hmm_db_hit_id, :sequence
   belongs_to :hmm_result_row
   belongs_to :hmm_db_hit
+  validates :hmm_result_row_id, presence: true
+  validates :hmm_db_hit_id, presence: true
+  validates :sequence, presence: true
+
+  def all_hits(sequence_db)
+    hmm_profiles = HmmProfile.all()
+    HmmProfiles.all do |profile|
+      profile.hmm_results do |result|
+        
+      end
+    end
+  end
 end
