9931416f8fd7918bc92aaa6f71dc0e67e503b022
Johannes Alneberg <alneberg@kth.se>
2012-05-30 12:52:39 +0200

    Removed old controllers

--- app/controllers/result_rows_controller.rb
+++
@@ -1,8 +0,0 @@
-class ResultRowsController < ApplicationController
-  def show
-    @result_row = ResultRow.find(params[:id])
-    @result= Result.find(@result_row.result_id)
-    @profile= Profile.find(@result.profile.id)
-    @sequences = @result_row.sequences.paginate(page: params[:page])
-  end
-end
--- app/controllers/sequences_controller.rb
+++
@@ -1,9 +0,0 @@
-class SequencesController < ApplicationController
-  def show
-    @sequence = Sequence.find(params[:id])
-  end
-  
-  def index
-    @sequences = Sequence.paginate(page: params[:page])
-  end
-end
