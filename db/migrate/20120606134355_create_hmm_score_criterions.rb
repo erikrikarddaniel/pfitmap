da80d416ed25a63c640de084b0613ef7ffd90e23
Johannes Alneberg <alneberg@kth.se>
2012-06-06 15:48:30 +0200

    Finished migration and ran ./migrate.sh

--- db/migrate/20120606134355_create_hmm_score_criterions.rb
+++ db/migrate/20120606134355_create_hmm_score_criterions.rb
@@ -1,7 +1,9 @@
 class CreateHmmScoreCriterions < ActiveRecord::Migration
   def change
     create_table :hmm_score_criterions do |t|
-
+      t.float :min_fullseq_score
+      t.integer :inclusion_criterion_id
+      
       t.timestamps
     end
   end
--- spec/models/hmm_score_criterion_spec.rb
+++ spec/models/hmm_score_criterion_spec.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: hmm_score_criterions
+#
+#  id                     :integer         not null, primary key
+#  min_fullseq_score      :float
+#  inclusion_criterion_id :integer
+#  created_at             :datetime        not null
+#  updated_at             :datetime        not null
+#
+
 require 'spec_helper'
 
 describe HmmScoreCriterion do
