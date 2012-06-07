e8de14e70efd39b443624f2fe9e8efca1bab7aad
Johannes Alneberg <alneberg@kth.se>
2012-06-07 16:07:42 +0200

    Added validation for uniqueness of parent

--- app/models/hmm_score_criterion.rb
+++ app/models/hmm_score_criterion.rb
@@ -12,10 +12,13 @@
 class HmmScoreCriterion < ActiveRecord::Base
   attr_accessible :min_fullseq_score, :inclusion_criterion_id
   belongs_to :inclusion_criterion
-  validates :inclusion_criterion, :presence => :true
+  validates :inclusion_criterion_id, :presence => :true, :uniqueness => :true
+  validates :min_fullseq_score, :presence => :true
 
+  #Assumes best hmm profile for db_sequence is the profile that 
+  # inclusion criterion belongs to.
   def evaluate?(db_sequence)
     score = db_sequence.best_hmm_result_row.fullseq_score
-    return (fullseq_score > self.min_fullseq_score)
+    return (score > self.min_fullseq_score)
   end
 end
