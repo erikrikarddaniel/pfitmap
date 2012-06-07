3a0fcdebd402d3f908b1de0b993c06a335c738b3
Johannes Alneberg <alneberg@kth.se>
2012-06-07 13:03:37 +0200

    checked out the experimental migration

--- db/migrate/20120606112354_create_inclusion_criterions.rb
+++ db/migrate/20120606112354_create_inclusion_criterions.rb
@@ -2,9 +2,11 @@ class CreateInclusionCriterions < ActiveRecord::Migration
   def change
     create_table :inclusion_criterions do |t|
       t.integer :hmm_profile_id
+      t.float :min_fullseq_score
+      t.string :type
 
       t.timestamps
     end
-    add_index :inclusion_criterions, :hmm_profile_id
+    add_index :inclusion_criterions, [:hmm_profile_id, :type]
   end
 end
