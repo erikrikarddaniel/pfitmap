e14faada6f9d1e52cf4c99cc913655d98a721b67
Johannes Alneberg <alneberg@kth.se>
2012-06-11 13:29:51 +0200

    Added validations for pfitmap release

--- app/models/pfitmap_release.rb
+++ app/models/pfitmap_release.rb
@@ -1,2 +1,18 @@
+# == Schema Information
+#
+# Table name: pfitmap_releases
+#
+#  id           :integer         not null, primary key
+#  release      :string(255)
+#  release_date :date
+#  current      :boolean
+#  created_at   :datetime        not null
+#  updated_at   :datetime        not null
+#
+
 class PfitmapRelease < ActiveRecord::Base
+  attr_accessible :release, :release_date, :current
+  validates :release, :presence => :true
+  validates :release_date, :presence => :true
+  validates_inclusion_of :current, :in => [true, false]
 end
--- spec/models/pfitmap_release_spec.rb
+++ spec/models/pfitmap_release_spec.rb
@@ -1,5 +1,43 @@
+# == Schema Information
+#
+# Table name: pfitmap_releases
+#
+#  id           :integer         not null, primary key
+#  release      :string(255)
+#  release_date :date
+#  current      :boolean
+#  created_at   :datetime        not null
+#  updated_at   :datetime        not null
+#
+
 require 'spec_helper'
 
 describe PfitmapRelease do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @pfitmap_release = PfitmapRelease.new(release: "0.1", release_date: "2001-04-20",current: false)
+  end
+  subject{ @pfitmap_release }
+
+  it { should be_valid }
+  it { should respond_to(:release) }
+  it { should respond_to(:release_date) }
+  it { should respond_to(:current) }
+  it { should respond_to(:pfitmap_sequences) }
+  it { should respond_to(:db_sequences) }
+
+  describe "without current" do
+    before{ @pfitmap_release.current = nil }
+    it { should_not be_valid }
+  end
+
+  describe "without release" do
+    before{ @pfitmap_release.release = nil }
+    it { should_not be_valid }
+  end
+  
+  describe "without release date" do
+    before{ @pfitmap_release.release_date = nil }
+    it { should_not be_valid }
+  end
+  
 end
