4e880b20a66764944e986730353c0f0444db2155
Johannes Alneberg <alneberg@kth.se>
2012-06-11 12:31:42 +0200

    Added columns to migration for pfitmap release

--- db/migrate/20120611101847_create_pfitmap_releases.rb
+++ db/migrate/20120611101847_create_pfitmap_releases.rb
@@ -1,8 +1,12 @@
 class CreatePfitmapReleases < ActiveRecord::Migration
   def change
     create_table :pfitmap_releases do |t|
-
+      t.string :release
+      t.date :release_date
+      t.boolean :current
+      
       t.timestamps
     end
+    add_index :pfitmap_releases, :current
   end
 end
