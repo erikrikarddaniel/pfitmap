3c6ac4e0a7e1e8c30024b60d08a6b5ee5106bd76
Johannes Alneberg <alneberg@kth.se>
2012-06-11 15:28:27 +0200

    Started with custom validation helper

--- app/helpers/pfitmap_releases_helper.rb
+++ app/helpers/pfitmap_releases_helper.rb
@@ -1,2 +1,17 @@
 module PfitmapReleasesHelper
+  def get_head_release
+    return PfitmapRelease.find_by_current(true)
+  end
+    
+
+  class CurrentReleaseValidator < ActiveModel::Validator
+    def validate(record)
+      current_release = get_head_release
+      if current_release
+        if record.current
+            record.errors[:base] << "There can only be one current release!"
+        end
+      end
+    end
+  end
 end
