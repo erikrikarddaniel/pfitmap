a9738b6f9e2eb942da320fe2bd2577cb0746f1b8
Johannes Alneberg <alneberg@kth.se>
2012-06-12 09:56:55 +0200

    Validation for pfitmap release, one current at a time

--- app/helpers/pfitmap_releases_helper.rb
+++ app/helpers/pfitmap_releases_helper.rb
@@ -1,13 +1,2 @@
 module PfitmapReleasesHelper
-  
-  class CurrentReleaseValidator < ActiveModel::Validator
-    def validate(record)
-      current_release = get_head_release
-      if current_release
-        if record.current
-            record.errors[:base] << "There can only be one current release!"
-        end
-      end
-    end
-  end
 end
--- app/models/pfitmap_release.rb
+++ app/models/pfitmap_release.rb
@@ -10,6 +10,7 @@
 #  updated_at   :datetime        not null
 #
 
+
 class PfitmapRelease < ActiveRecord::Base
   attr_accessible :release, :release_date, :current
   has_many :pfitmap_sequences
@@ -17,6 +18,7 @@ class PfitmapRelease < ActiveRecord::Base
   validates :release, :presence => :true
   validates :release_date, :presence => :true
   validates_inclusion_of :current, :in => [true, false]
+  validate :only_one_current
 
   # Should only be called when there exists a head release
   def self.add_seq_to_head(db_seq)
@@ -28,5 +30,15 @@ class PfitmapRelease < ActiveRecord::Base
     
   def self.get_head_release
       return find_by_current(true)
+  end
+
+  private 
+  def only_one_current
+    current_release = PfitmapRelease.get_head_release
+    if current_release
+      if self.current
+        self.errors[:base] << "There can only be one current release!"
+      end
+    end
   end
 end
