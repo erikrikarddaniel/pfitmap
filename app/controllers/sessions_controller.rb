8ce91fae2d6cf0de640618fcdd2b51388f504b15
Johannes Alneberg <alneberg@kth.se>
2012-06-19 17:18:20 +0200

    Added methods to sessions_controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,20 @@
 class SessionsController < ApplicationController
+  def create
+    reset_session # see http://guides.rubyonrails.org/security.html#session-fixation
+    info = request.env["omniauth.auth"]
+    sessions[:name] = info["user_info"]["name"] || info["user_info"]["email"] || info["user_info"]["nickname"] || "unknown friend"
+
+    redirect_to root, :notice => "Welcome #{session[:name]}!"
+   
+  end
+
+  def failure
+    redirect_to login_url :alert => 'Sorry, there was something wrong with your login attempt. Please try again.'
+  end
+
+  def destroy
+    reset_session
+    flash[:notice] = "Logged out."
+    redirect_to root
+  end
 end
