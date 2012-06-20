cfbba52c30e635a2dbb1182938a5bcf052e72fd0
Johannes Alneberg <alneberg@kth.se>
2012-06-20 14:06:34 +0200

    Added admin role to abilities

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,10 +2,12 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    if user.role? :admin
+    user ||= User.new(:role => 'guest')
+    if user.role == 'admin'
       can :manage, :all
     else
       can :read, :all
+      cannot :read [Users]
     end
   end
 end
