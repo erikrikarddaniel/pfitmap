3562265f087d479e075685b1e427b9f126fe061d
Johannes Alneberg <alneberg@kth.se>
2012-06-20 14:04:35 +0200

    Migrated role column to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120619140407) do
+ActiveRecord::Schema.define(:version => 20120620113306) do
 
   create_table "db_sequences", :force => true do |t|
     t.datetime "created_at", :null => false
@@ -123,6 +123,7 @@ ActiveRecord::Schema.define(:version => 20120619140407) do
     t.string   "email"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "role"
   end
 
 end
