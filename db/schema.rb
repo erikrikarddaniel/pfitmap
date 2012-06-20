5f2f73670f105677cd7f8c81a2782f35647304c1
Johannes Alneberg <alneberg@kth.se>
2012-06-20 09:36:16 +0200

    Migrated user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120618065947) do
+ActiveRecord::Schema.define(:version => 20120619140407) do
 
   create_table "db_sequences", :force => true do |t|
     t.datetime "created_at", :null => false
@@ -112,6 +112,15 @@ ActiveRecord::Schema.define(:version => 20120618065947) do
     t.string   "source"
     t.string   "name"
     t.string   "version"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "name"
+    t.string   "email"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
