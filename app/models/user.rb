d9b5d73d8d5e370582bd49ec1c5fb1fc6b73bff7
Johannes Alneberg <alneberg@kth.se>
2012-06-20 10:47:37 +0200

    Annotated user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer         not null, primary key
+#  provider   :string(255)
+#  uid        :string(255)
+#  name       :string(255)
+#  email      :string(255)
+#  created_at :datetime        not null
+#  updated_at :datetime        not null
+#
+
 class User < ActiveRecord::Base
   def self.create_with_omniauth(auth)
     create! do |user|
