0cf0f65ed46b18dfd334f6610a577f4b0633ff58
Johannes Alneberg <alneberg@kth.se>
2012-06-20 14:06:00 +0200

    Added roles to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,15 +9,19 @@
 #  email      :string(255)
 #  created_at :datetime        not null
 #  updated_at :datetime        not null
+#  role       :string(255)
 #
 
 class User < ActiveRecord::Base
+  ROLES = %w[admin guest]
+
   def self.create_with_omniauth(auth)
     create! do |user|
       user.provider = auth["provider"]
       user.uid = auth["uid"]
       user.name = auth["info"]["name"]
       user.email = auth["info"]["email"]
+      user.role = 'guest'
     end
   end
 end
