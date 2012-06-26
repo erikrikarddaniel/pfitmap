28a703ad0a077f5ba9f280fa70768f31c6ebf316
Johannes Alneberg <alneberg@kth.se>
2012-06-26 16:47:47 +0200

    Removed new and create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,37 +22,12 @@ class UsersController < ApplicationController
     end
   end
 
-  # GET /users/new
-  # GET /users/new.json
-  def new
-    @user = User.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @user }
-    end
-  end
 
   # GET /users/1/edit
   def edit
     @user = User.find(params[:id])
   end
 
-  # POST /users
-  # POST /users.json
-  def create
-    @user = User.new(params[:user])
-
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render json: @user, status: :created, location: @user }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
-  end
 
   # PUT /users/1
   # PUT /users/1.json
