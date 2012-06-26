02fc678a73d55b39df6ed0cd944dbb28b27b5873
Johannes Alneberg <alneberg@kth.se>
2012-06-26 11:08:18 +0200

    rescue from authorization error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,11 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
   check_authorization
 
+  rescue_from CanCan::AccessDenied do |exception|
+    flash[:error] = exception.message
+    redirect_to root_url
+  end
+
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
