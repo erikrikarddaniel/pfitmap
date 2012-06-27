50d5837d577496b01da8951d58ede46827f415d5
Johannes Alneberg <alneberg@kth.se>
2012-06-27 17:07:49 +0200

    Session and variables for controller spec

--- spec/controllers/hmm_score_criteria_controller_spec.rb
+++ spec/controllers/hmm_score_criteria_controller_spec.rb
@@ -19,19 +19,24 @@ require 'spec_helper'
 # that an instance is receiving a specific message.
 
 describe HmmScoreCriteriaController do
+  before do
+    @user = get_admin_user
+  end
+  let!(:hmm_profile) { FactoryGirl.create(:hmm_profile) }
 
   # This should return the minimal set of attributes required to create a valid
   # HmmScoreCriterion. As you add validations to HmmScoreCriterion, be sure to
   # update the return value of this method accordingly.
   def valid_attributes
-    {}
+    {hmm_profile_id: hmm_profile.id,
+    min_fullseq_score: 10.0}
   end
   
   # This should return the minimal set of values that should be in the session
   # in order to pass any filters (e.g. authentication) defined in
   # HmmScoreCriteriaController. Be sure to keep this updated too.
   def valid_session
-    {}
+    {user_id: @user.id}
   end
 
   describe "GET index" do
