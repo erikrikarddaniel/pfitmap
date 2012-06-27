d8cbab83350558eae968e75076913b357058173b
Johannes Alneberg <alneberg@kth.se>
2012-06-27 14:21:33 +0200

    Enabled pluralization to criteria

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,8 @@
-#Inflector.inflections do |inflection| inflection.irregular "criterion", "criteria"
-
 # Load the rails application
 require File.expand_path('../application', __FILE__)
+
+ActiveSupport::Inflector.inflections do |inflection| inflection.irregular "criterion", "criteria"
+end
 
 # Initialize the rails application
 Pfitmap::Application.initialize!
