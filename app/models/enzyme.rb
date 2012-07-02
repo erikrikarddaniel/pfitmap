b2341dee4d5a32b144fdc472a8a0307923b73566
Johannes Alneberg <alneberg@kth.se>
2012-07-02 11:55:23 +0200

    Destroying enzyme should destroy relation

--- app/models/enzyme.rb
+++ app/models/enzyme.rb
@@ -10,6 +10,6 @@
 
 class Enzyme < ActiveRecord::Base
   attr_accessible :name
-  has_many :enzyme_profiles
+  has_many :enzyme_profiles, dependent: :destroy
   has_many :hmm_profiles, through: :enzyme_profiles
 end
