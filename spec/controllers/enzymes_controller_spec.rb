ed4bdfa20c8bc5f4b9a07270f5072c145f0c0ce9
Johannes Alneberg <alneberg@kth.se>
2012-07-02 13:21:37 +0200

    Enzyme controller spec as admin

--- spec/controllers/enzymes_controller_spec.rb
+++ spec/controllers/enzymes_controller_spec.rb
@@ -19,19 +19,21 @@ require 'spec_helper'
 # that an instance is receiving a specific message.
 
 describe EnzymesController do
-
+  before do
+    @user = get_admin_user
+  end
   # This should return the minimal set of attributes required to create a valid
   # Enzyme. As you add validations to Enzyme, be sure to
   # update the return value of this method accordingly.
   def valid_attributes
-    {}
+    {name: "exname"}
   end
   
   # This should return the minimal set of values that should be in the session
   # in order to pass any filters (e.g. authentication) defined in
   # EnzymesController. Be sure to keep this updated too.
   def valid_session
-    {}
+    {user_id: @user.id}
   end
 
   describe "GET index" do
