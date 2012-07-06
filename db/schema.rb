a4b760441c48e86cdd2f1aa8e8b2bfddd5586ed2
Johannes Alneberg <alneberg@kth.se>
2012-07-06 13:38:28 +0200

    db:rollback two steps

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120705144027) do
+ActiveRecord::Schema.define(:version => 20120702134627) do
 
   create_table "db_sequences", :force => true do |t|
     t.datetime "created_at", :null => false
@@ -99,23 +99,12 @@ ActiveRecord::Schema.define(:version => 20120705144027) do
     t.index ["hmm_profile_id"], :name => "index_hmm_score_criterions_on_hmm_profile_id"
   end
 
-  create_table "sequence_sources", :force => true do |t|
-    t.string   "source"
-    t.string   "name"
-    t.string   "version"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
-
   create_table "pfitmap_releases", :force => true do |t|
     t.string   "release"
     t.date     "release_date"
-    t.datetime "created_at",         :null => false
-    t.datetime "updated_at",         :null => false
+    t.datetime "created_at",   :null => false
+    t.datetime "updated_at",   :null => false
     t.boolean  "current"
-    t.integer  "sequence_source_id"
-    t.index ["sequence_source_id"], :name => "index_pfitmap_releases_on_sequence_source_id"
-    t.foreign_key ["sequence_source_id"], "sequence_sources", ["id"], :on_update => :no_action, :on_delete => :no_action, :name => "pfitmap_releases_sequence_source_id_fkey"
   end
 
   create_table "pfitmap_sequences", :force => true do |t|
@@ -125,6 +114,14 @@ ActiveRecord::Schema.define(:version => 20120705144027) do
     t.integer  "pfitmap_release_id"
     t.index ["db_sequence_id"], :name => "index_pfitmap_sequences_on_db_sequence_id"
     t.index ["pfitmap_release_id"], :name => "index_pfitmap_sequences_on_pfitmap_release_id"
+  end
+
+  create_table "sequence_sources", :force => true do |t|
+    t.string   "source"
+    t.string   "name"
+    t.string   "version"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "users", :force => true do |t|
@@ -137,5 +134,4 @@ ActiveRecord::Schema.define(:version => 20120705144027) do
     t.string   "role"
   end
 
-  create_view "view_db_sequence_best_profiles", "SELECT hmmp.id AS hmm_profile_id, ss.id AS sequence_source_id, dbs.id AS db_sequence_id, hmmrr.id AS hmm_result_row_id, hmmrr.fullseq_score FROM ((((db_sequences dbs JOIN hmm_result_rows hmmrr ON ((dbs.id = hmmrr.db_sequence_id))) JOIN hmm_results hmmr ON ((hmmrr.hmm_result_id = hmmr.id))) JOIN hmm_profiles hmmp ON ((hmmr.hmm_profile_id = hmmp.id))) JOIN sequence_sources ss ON ((hmmr.sequence_source_id = ss.id))) WHERE (hmmrr.fullseq_score = (SELECT max(hmmrrinner.fullseq_score) AS max FROM ((hmm_result_rows hmmrrinner JOIN hmm_results hmmrinner ON ((hmmrrinner.hmm_result_id = hmmrinner.id))) JOIN sequence_sources ssinner ON ((hmmrinner.sequence_source_id = ssinner.id))) WHERE ((hmmrrinner.db_sequence_id = dbs.id) AND (ssinner.id = ss.id))))", :force => true
 end
