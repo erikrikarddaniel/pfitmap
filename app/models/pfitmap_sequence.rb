d4e93a30f4b0450add756b32dc869af1d5ac2478
Johannes Alneberg <alneberg@kth.se>
2012-07-13 15:52:04 +0200

    db_hits_from_ref method in pfitmap_sequences

--- app/models/pfitmap_sequence.rb
+++ app/models/pfitmap_sequence.rb
@@ -15,6 +15,7 @@ class PfitmapSequence < ActiveRecord::Base
   belongs_to :db_sequence
   belongs_to :pfitmap_release
   belongs_to :hmm_profile
+  has_many :hmm_db_hits, :through => :db_sequence
   has_one :sequence_source, :through => :pfitmap_release
 
   # calculate_counts(pr : pfitmap_release)
@@ -27,7 +28,7 @@ class PfitmapSequence < ActiveRecord::Base
         if profile.enzymes != []
           profile.enzymes.each do |enzyme|
             protein = Protein.add_if_not_existing(enzyme,profile)
-            Taxon.add_if_not_existing(ref_hit.all_gold_taxons)
+            Taxon.add_all_if_not_existing(ref_hit.all_gold_taxons)
             ProteinCount.add_to_count(protein, ref_hit.all_gold_taxons, pr)
           end
         else
@@ -36,5 +37,9 @@ class PfitmapSequence < ActiveRecord::Base
         end
       end
     end
+  end
+
+  def db_hits_from_ref
+    self.hmm_db_hits.where("db = 'ref'")
   end
 end
