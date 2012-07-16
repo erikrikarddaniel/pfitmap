74c392e5c527b65aa8807a96a5d14786bc5523a5
Johannes Alneberg <alneberg@kth.se>
2012-07-16 10:01:46 +0200

    Removed index due to migration crash

--- db/migrate/20120713150839_create_proteins.rb
+++ db/migrate/20120713150839_create_proteins.rb
@@ -3,12 +3,10 @@ class CreateProteins < ActiveRecord::Migration
     create_table :proteins do |t|
       t.string :name
       t.string :rank
-      t.references :HmmProfile
-      t.references :Enzyme
+      t.integer :hmm_profile_id
+      t.integer :enzyme_id
 
       t.timestamps
     end
-    add_index :proteins, :HmmProfile_id
-    add_index :proteins, :Enzyme_id
   end
 end
