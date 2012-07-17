e075ba19800fd3e7fb701cacc1b53c511faa97d8
Johannes Alneberg <alneberg@kth.se>
2012-07-17 14:23:14 +0200

    Changed the taxons original migration to include ncbi_taxon_id as primary key

--- db/migrate/20120713143558_create_taxons.rb
+++ db/migrate/20120713143558_create_taxons.rb
@@ -1,11 +1,14 @@
 class CreateTaxons < ActiveRecord::Migration
   def change
-    create_table :taxons do |t|
+    create_table :taxons, :id => false do |t|
+      t.integer :ncbi_taxon_id, :primary_key
       t.string :name
       t.string :rank
       t.boolean :wgs
+      t.integer :pfitmap_release_id
 
       t.timestamps
     end
+    add_index :taxons, :pfitmap_release_id
   end
 end
