e11da6feb4fd72062c51aca2f84fe2bbf1fdf508
Johannes Alneberg <alneberg@kth.se>
2012-07-17 17:20:58 +0200

    Annotation differently due to changed migration

--- app/models/taxon.rb
+++ app/models/taxon.rb
@@ -2,14 +2,13 @@
 #
 # Table name: taxons
 #
-#  id                 :integer         not null, primary key
-#  ncbi_taxon_id      :integer
-#  name               :string(255)
-#  rank               :string(255)
-#  wgs                :boolean
-#  pfitmap_release_id :integer
-#  created_at         :datetime        not null
-#  updated_at         :datetime        not null
+#  id            :integer         not null, primary key
+#  ncbi_taxon_id :integer
+#  name          :string(255)
+#  rank          :string(255)
+#  wgs           :boolean
+#  created_at    :datetime        not null
+#  updated_at    :datetime        not null
 #
 
 class Taxon < ActiveRecord::Base
