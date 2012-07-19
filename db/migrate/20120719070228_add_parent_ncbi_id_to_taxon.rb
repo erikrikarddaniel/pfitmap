6376d344108fa21ed4fd959e30d32c4530e8aad6
Johannes Alneberg <alneberg@kth.se>
2012-07-19 09:16:23 +0200

    Add index on the column referenced by the foreign key

--- db/migrate/20120719070228_add_parent_ncbi_id_to_taxon.rb
+++ db/migrate/20120719070228_add_parent_ncbi_id_to_taxon.rb
@@ -1,5 +1,6 @@
 class AddParentNcbiIdToTaxon < ActiveRecord::Migration
   def change
+    add_index :taxons, :ncbi_taxon_id, :unique => true
     add_column :taxons, :parent_ncbi_id, :integer, :references => [:taxons, :ncbi_taxon_id]
 
   end
