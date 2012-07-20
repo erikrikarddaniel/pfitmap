cf95539a9cd4e2339c1b9b89a1a4cb5156f56327
Johannes Alneberg <alneberg@kth.se>
2012-07-20 18:24:21 +0200

    Added new methods and obs_as_genome to protein_count

--- app/models/protein_count.rb
+++ app/models/protein_count.rb
@@ -11,9 +11,11 @@
 #  taxon_id                 :integer
 #  created_at               :datetime        not null
 #  updated_at               :datetime        not null
+#  obs_as_genome            :boolean
 #
 
 class ProteinCount < ActiveRecord::Base
+  attr_accessible :no_genomes, :no_proteins, :no_genomes_with_proteins, :obs_as_genome
   belongs_to :protein
   belongs_to :pfitmap_release
   belongs_to :taxon
@@ -22,5 +24,27 @@ class ProteinCount < ActiveRecord::Base
     self.no_genomes += 1
     self.save
   end
-    
+  
+  def self.add_hit(protein, taxons, pr)
+    # Check if the genome refferred to have got a hit from before.
+    # If not, then this is the first out of possibly many proteins
+    # to hit this protein_count.
+    first_taxon = taxons.first
+    first_protein_count = self.find(:first, :conditions => ["protein_id = ? AND taxon_id = ? AND pfitmap_release_id = ?", protein.id, first_taxon.id, pr.id])
+    first_protein = !(first_protein_count.obs_as_genome)
+    if first_protein
+      first_protein_count.obs_as_genome = true
+      first_protein_count.save
+    end
+        
+    taxons.each do |taxon|
+
+      protein_count = self.find(:first, :conditions => ["protein_id = ? AND taxon_id = ? AND pfitmap_release_id = ?", protein.id, taxon.id, pr.id])
+      if first_protein
+        protein_count.no_genomes_with_proteins += 1
+      end
+      protein_count.no_proteins += 1
+      protein_count.save
+    end
+  end
 end
