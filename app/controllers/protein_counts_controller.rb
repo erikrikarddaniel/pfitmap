11e019eaf27cd6c327f4ad5550f8283991f8269f
Johannes Alneberg <alneberg@kth.se>
2012-07-24 18:09:17 +0200

    started on protein_counts index chosing rank

--- app/controllers/protein_counts_controller.rb
+++ app/controllers/protein_counts_controller.rb
@@ -3,8 +3,11 @@ class ProteinCountsController < ApplicationController
   # GET /protein_counts
   # GET /protein_counts.json
   def index
-    @protein_counts = ProteinCount.all
-
+    if params[:rank]
+      @protein_counts = ProteinCount.from_rank(params[:rank]).paginate(:page => params[:page])
+    else
+      @protein_counts = ProteinCount.paginate(:page => params[:page])
+    end
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @protein_counts }
