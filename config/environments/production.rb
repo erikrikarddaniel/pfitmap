671c3ca426b1cde2fbd0d0ff9d9f0702de43acc9
Johannes Alneberg <alneberg@kth.se>
2012-07-30 17:11:33 +0200

    Fixed typo in precompile list

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,7 @@ Pfitmap::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += ['expandable_list.js, expandable_ajax_list.js, expandable_summary_table.js, custom.css.scss, bootstrap-dropdown.js']
+  config.assets.precompile += ['expandable_list.js', 'expandable_ajax_list.js', 'expandable_summary_table.js', 'custom.css.scss', 'bootstrap-dropdown.js']
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
