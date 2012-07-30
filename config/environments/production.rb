de1d71e718b978cbad20bb28e2e2f0978a470f1a
Johannes Alneberg <alneberg@kth.se>
2012-07-30 16:45:58 +0200

    Changed back to fallback compile and added custom js and css to precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ Pfitmap::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = true
+  config.assets.compile = false
 
   # Generate digests for assets URLs
   config.assets.digest = true
@@ -46,7 +46,7 @@ Pfitmap::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += ['expandable_list.js']
+  config.assets.precompile += ['expandable_list.js, expandable_ajax_list.js, expandable_summary_table.js, custom.css.scss, bootstrap-dropdown.js']
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
