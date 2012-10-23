63bc069b0ffd92387c5cb9f03ba5c9e62b5e2f74
Johannes Alneberg <alneberg@kth.se>
2012-10-23 15:19:12 +0200

    New names for biosql urls

--- app/models/biosql_web.rb
+++ app/models/biosql_web.rb
@@ -4,18 +4,18 @@ class BiosqlWeb < ActiveRecord::Base
 
   def self.get_taxons_ncbi_id_by_gi(gi)
     options = {:headers => { 'Content-Type' => 'application/json', 'Accepts' => 'application/json'}, :body => {:gi => gi}.to_json}
-    response = HTTParty.get(BASE_URL + '/taxons_ncbi_for_gi.json', options)
+    response = HTTParty.get(BASE_URL + '/gi2ncbi_taxon_id.json', options)
     taxons = response.parsed_response
   end
 
   def self.wgs_ncbi_ids
-    response = HTTParty.get(BASE_URL + '/wgs_ncbi_ids.json')
+    response = HTTParty.get(BASE_URL + '/wgs_ncbi_taxon_ids.json')
     wgs_ids = response.parsed_response
   end
 
   def self.full_taxa_for_ncbi_id(wgs_id)
     options = {:headers => { 'Content-Type' => 'application/json', 'Accepts' => 'application/json'}, :body => {:id => wgs_id}.to_json}
-    response = HTTParty.get(BASE_URL + '/full_taxa_for_ncbi_id.json', options)
+    response = HTTParty.get(BASE_URL + '/ncbi_taxon_id2full_taxon.json', options)
     taxons = response.parsed_response
   end
 end
