39770b168d972f6f2bf52adfb18eebd8345781df
Johannes Alneberg <alneberg@kth.se>
2012-11-13 15:42:18 +0100

    Named profile factories initialize corresponding enzymes aswell

---
+++ spec/factories/enzymes.rb
@@ -0,0 +1,13 @@
+FactoryGirl.define do
+  factory :enzyme_class_1, class: Enzyme do
+    name "RNR class I enzyme"
+  end
+
+  factory :enzyme_class_2, class: Enzyme do
+    name "RNR class II enzyme"
+  end
+
+  factory :enzyme_class_3, class: Enzyme do
+    name "RNR class III enzyme"
+  end
+end
