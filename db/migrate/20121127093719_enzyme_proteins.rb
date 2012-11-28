7f17eee686484aba4f1dc852fa85acf6f21cfe4b
Johannes Alneberg <alneberg@kth.se>
2012-11-28 13:45:25 +0100

    Migration changed to be able to rollback

--- db/migrate/20121127093719_enzyme_proteins.rb
+++ db/migrate/20121127093719_enzyme_proteins.rb
@@ -1,5 +1,5 @@
 class EnzymeProteins < ActiveRecord::Migration
-  def change
+  def up
     remove_column :proteins, :enzyme_id
     create_table :enzyme_proteins do |t|
       t.integer :enzyme_id
@@ -7,5 +7,9 @@ class EnzymeProteins < ActiveRecord::Migration
 
       t.timestamps
     end
+  end
+  def down
+    drop_table :enzyme_proteins
+    add_column :proteins, :enzyme_id, :integer
   end
 end
