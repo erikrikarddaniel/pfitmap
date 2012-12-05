bedcc8c78302abe3fa56de5d3eb1e148fe5d2dcf
Johannes Alneberg <alneberg@kth.se>
2012-12-05 17:31:20 +0100

    Removed old protein_count routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Pfitmap::Application.routes.draw do
 
-  get 'protein_counts_by_rank', to: 'protein_counts#by_rank'
   get 'protein_counts_with_enzymes', to: 'protein_counts#with_enzymes'
-  get 'protein_counts_by_hierarchy', to: 'protein_counts#by_hierarchy'
+  post 'protein_counts_with_enzymes', to: 'protein_counts#with_enzymes'
   get 'add_row', to: 'protein_counts#add_row'
   get 'collapse_rows', to: 'protein_counts#collapse_rows'
   
