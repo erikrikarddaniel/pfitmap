52b7e857e30e17c84a03f7eeaff9ea9e8c52bf2f
Brynjar Smari Bjarnason <binni@binnisb.com>
2013-08-13 15:29:32 +0200

    links and javascript

--- data/d3-prototype/scripts.js
+++ data/d3-prototype/scripts.js
@@ -1,19 +1,27 @@
 /**
  * @author Brynjar Smari Bjarnason
  */
-function div_bar() {
-	var dataset;
+var dataset;
+
+function load_data() {
 	d3.tsv("d3-prototype.static_matrix.n100.tsv", function(data) {
-		dataset = data;
-		console.log(dataset);
-		d3.select("body").selectAll("div")
-			.data(dataset)
-			.enter()
-			.append("div")
-			.attr("class","bar")
-			.style("height",function(d) {var bar_height = d.no_proteins * 10; return bar_height + "px"});
+	dataset = data;
 	});
 };
+
+
+function div_bar() {
+	d3.select("body").selectAll("div")
+		.data(dataset)
+		.enter()
+		.append("div")
+		.attr("class","bar")
+		.style("height",function(d) {var bar_height = d.no_proteins * 10; return bar_height + "px"});
+};
 function sum_organism(level) {
+	console.log(level);
+	console.log(dataset[0].no_genomes)
+};
+function sum_protein(level) {
 	alert(level);
 };
