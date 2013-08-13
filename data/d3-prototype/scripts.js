d70a1a5187d9b9a98e32a2c1ca220b5b23cee06d
Brynjar Smari Bjarnason <binni@binnisb.com>
2013-08-13 16:17:14 +0200

    print the data matrix, missing the column and row names though.

--- data/d3-prototype/scripts.js
+++ data/d3-prototype/scripts.js
@@ -4,11 +4,17 @@
 var dataset;
 
 function load_data() {
-	d3.tsv("d3-prototype.static_matrix.n100.tsv", function(data) {
+	d3.tsv("column_matrix_top_protein_level.tsv", function(data) {
 	dataset = data;
+	table_it();
 	});
 };
 
+function table_it() {
+	console.log(dataset);
+	var tr = d3.select("body").append("table").selectAll("tr").data(dataset).enter().append("tr");
+	var td = tr.selectAll("td").data(function(d) {return [d.n_genomes, d["protein:RNR I:NrdA:n_proteins"], d["protein:RNR I:NrdA:n_genomes_w_protein"], d["protein:RNR I:NrdB:n_proteins"], d["protein:RNR I:NrdB:n_genomes_w_protein"], d["protein:RNR III:NrdD:n_proteins"], d["protein:RNR III:NrdD:n_genomes_w_protein"], d["protein:RNR III:NrdG:n_proteins"], d["protein:RNR III:NrdG:n_genomes_w_protein"],d["protein:RNR II:NrdJ:n_proteins"],d["protein:RNR II:NrdJ:n_genomes_w_protein"] ]; }).enter().append("td").text(function(d) { return d; });
+}
 
 function div_bar() {
 	d3.select("body").selectAll("div")
