bbdd073947bd30f3a2bd107680b0a4689849e4b4
Brynjar Smari Bjarnason <binni@binnisb.com>
2013-08-23 13:12:17 +0200

    got d3 circos working, no labels though

--- data/d3-prototype/scripts.js
+++ data/d3-prototype/scripts.js
@@ -10,7 +10,7 @@ var pfitmap = {
 	organisms_count : null,
 	columns_names : [],
 	proteins_names : [],
-
+	circos_matrix : [],
 	taxa_names : ["domain", "kingdom","phylum", "class", "order", "family", "genus", "species", "strain"],
 	taxa_level : "domain",
 	base_data_file : "column_matrix_top_protein_level.tsv"
@@ -77,7 +77,8 @@ function load_data(query) {
 };
 
 function table_it() {
-	d3.select("#heat_map").select("table").remove()
+	pfitmap.circos_matrix = [];
+	d3.select("#heat_map").select("table").remove();
 	var table = d3.select("#heat_map")
 		.append("table");
 		
@@ -94,11 +95,18 @@ function table_it() {
 		.data(pfitmap.dataset)
 		.enter()
 		.append("tr");
-		
+	var nr_rows = rows[0].length;
+	var matrix_size = rows[0].length + pfitmap.proteins_names.length;
+	for (var i = 0; i<matrix_size;i++) {
+		pfitmap.circos_matrix[i] =  Array.apply(null, new Array(matrix_size)).map(Number.prototype.valueOf,0);
+	}
+	
 	var cells = rows.selectAll("td")
-		.data(function(row) {
+		.data(function(row,i) {
+			
 			return pfitmap.columns_names.map(function(column) {
-				if (pfitmap.proteins_names.indexOf(column) != -1){ return { column: column, value: row[column], value_text: [row[column]["n_proteins"], row[column]["n_genomes_w_protein"]].join(" | ") } }
+				var protein_ind = pfitmap.proteins_names.indexOf(column);
+				if (protein_ind != -1){ pfitmap.circos_matrix[i][nr_rows+protein_ind] = +row[column]["n_genomes_w_protein"];pfitmap.circos_matrix[nr_rows+protein_ind][i] = +row[column]["n_genomes_w_protein"]; return { column: column, value: row[column], value_text: [row[column]["n_genomes_w_protein"],row[column]["n_proteins"]].join(" | ") } }
 				else { return {column: column, value_text: row[column]} }
 			})		
 		})
@@ -147,4 +155,91 @@ function sumArrays(group) {
 
 function getProteinName(protein) {
 	return protein.split(":").slice(1,-1).join(":")
+}
+
+function generate_circos() {
+var chord = d3.layout.chord()
+    .padding(.05)
+    .sortSubgroups(d3.descending)
+    .matrix(pfitmap.circos_matrix);
+
+var width = 960,
+    height = 500,
+    innerRadius = Math.min(width, height) * .41,
+    outerRadius = innerRadius * 1.1;
+
+var range9 = ["#000000", "#33585e", "#957244", "#F26223", "#155420", "#FF0000","#FFFF00","#FF00FF","#00FFFF"] 
+var fill = d3.scale.ordinal()
+    .domain(d3.range(range9.length))
+    .range(range9);
+
+var svg = d3.select("#circos_svg").append("svg")
+    .attr("width", width)
+    .attr("height", height)
+  .append("g")
+    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+svg.append("g").selectAll("path")
+    .data(chord.groups)
+  .enter().append("path")
+    .style("fill", function(d) { return fill(d.index); })
+    .style("stroke", function(d) { return fill(d.index); })
+    .attr("d", d3.svg.arc().innerRadius(innerRadius).outerRadius(outerRadius))
+    .on("mouseover", fade(.1))
+    .on("mouseout", fade(1));
+/*
+var ticks = svg.append("g").selectAll("g")
+    .data(chord.groups)
+  .enter().append("g").selectAll("g")
+    .data(groupTicks)
+  .enter().append("g")
+    .attr("transform", function(d) {
+      return "rotate(" + (d.angle * 180 / Math.PI - 90) + ")"
+          + "translate(" + outerRadius + ",0)";
+    });
+
+ticks.append("line")
+    .attr("x1", 1)
+    .attr("y1", 0)
+    .attr("x2", 5)
+    .attr("y2", 0)
+    .style("stroke", "#000");
+
+ticks.append("text")
+    .attr("x", 8)
+    .attr("dy", ".35em")
+    .attr("transform", function(d) { return d.angle > Math.PI ? "rotate(180)translate(-16)" : null; })
+    .style("text-anchor", function(d) { return d.angle > Math.PI ? "end" : null; })
+    .text(function(d) { return d.label; });
+*/
+svg.append("g")
+    .attr("class", "chord")
+  .selectAll("path")
+    .data(chord.chords)
+  .enter().append("path")
+    .attr("d", d3.svg.chord().radius(innerRadius))
+    .style("fill", function(d) { return fill(d.target.index); })
+    .style("opacity", 1);
+
+}
+
+// Returns an array of tick angles and labels, given a group.
+function groupTicks(d) {
+  var k = (d.endAngle - d.startAngle) / d.value;
+  return d3.range(0, d.value, 1000).map(function(v, i) {
+    return {
+      angle: v * k + d.startAngle,
+      label: i % 5 ? null : v / 1000 + "k"
+    };
+  });
+}
+
+// Returns an event handler for fading a given chord group.
+function fade(opacity) {
+  return function(g, i) {
+    svg.selectAll(".chord path")
+        .filter(function(d) { return d.source.index != i && d.target.index != i; })
+      .transition()
+        .style("opacity", opacity);
+  };
 }
