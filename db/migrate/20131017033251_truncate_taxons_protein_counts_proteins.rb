40e5a931e6f2fa71eb87e21a8599ddbd978ccd13
Brynjar Smari Bjarnason <binni@binnisb.com>
2013-10-17 04:19:55 +0200

    migration for truncating taxons, proteins, protein_counts and enzyme_proteins, closes #65

---
+++ db/migrate/20131017033251_truncate_taxons_protein_counts_proteins.rb
@@ -0,0 +1,5 @@
+class TruncateTaxonsProteinCountsProteins < ActiveRecord::Migration
+  def change
+    ActiveRecord::Base.connection.execute("TRUNCATE protein_counts, taxons, proteins, enzyme_proteins")
+  end
+end
