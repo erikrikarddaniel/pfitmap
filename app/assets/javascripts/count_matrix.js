11317ae13a3d3815c375596a4721ad6942f36fb6
Brynjar Smari Bjarnason <binni@binnisb.com>
2013-10-24 15:15:23 +0200

    made tooltip look a bit better

--- app/assets/javascripts/count_matrix.js
+++ app/assets/javascripts/count_matrix.js
@@ -124,9 +124,9 @@ function d3_table_it(dataset) {
         tlabel.append("input").attr("type","checkbox").attr("name","tax_filter").attr("value",function(d) {return d[gon.dataset.taxon_level]})
         tlabel.append("span").attr("class","input-group-addon").text(function(d) {return d[gon.dataset.taxon_level]})
     //taxon tooltip
-    $(".taxon_label").tooltip({"toggle":true,"title":function () {r=[];for (var i = 0; i < gon.taxon_levels.length-1; i++) {r.push(this.__data__[gon.taxon_levels[i]])};return r.join("\\")},"placement":"left"});
+    $(".taxon_label").tooltip({"toggle":true,"title":function () {r=[];for (var i = 0; i < gon.taxon_levels.length; i++) {r.push(this.__data__[gon.taxon_levels[i]])};return r.join("<br/>")},"placement":"left"});
     //cell tooltip
-    $(".heat_label").tooltip({"toggle":true,"title":function() {return "Proteins: "+this.__data__.value+" Genomes w. proteins: "+this.__data__.no_genomes_with_proteins+" Organism: " + this.__data__.organism + " Protein: "+this.__data__.column} })
+    $(".heat_label").tooltip({"toggle":true,"title":function() {return "Proteins: "+this.__data__.value+"<br/>Genomes w. proteins: "+this.__data__.no_genomes_with_proteins+"<br/>Organism: " + this.__data__.organism + "<br/>Protein: "+this.__data__.column} })
     d3_color_table(gon.taxon_levels[0]);
 }
 
