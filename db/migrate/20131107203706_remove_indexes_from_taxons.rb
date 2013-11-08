d5d293b63805e9853359fad8cd33825b19388969
Brynjar Smari Bjarnason <binni@binnisb.com>
2013-11-08 09:42:32 +0100

    index taxhierarchy not on remote db

--- db/migrate/20131107203706_remove_indexes_from_taxons.rb
+++ db/migrate/20131107203706_remove_indexes_from_taxons.rb
@@ -1,7 +1,6 @@
 class RemoveIndexesFromTaxons < ActiveRecord::Migration
   def up 
    remove_index(:taxons, :name => 'index_taxons_on_ncbi_taxon_id')
-   remove_index(:taxons, :name => 'taxhierarchy')
   end
 
 end
