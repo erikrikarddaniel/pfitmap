62e3889e8dcdc303632b088a186aa1deaab55cc1
Brynjar Smari Bjarnason <binni@binnisb.com>
2013-11-08 01:12:56 +0100

    Added basic tests for pfitmap release calculate released db.

--- spec/models/pfitmap_release_spec.rb
+++ spec/models/pfitmap_release_spec.rb
@@ -139,68 +139,63 @@ describe PfitmapRelease do
     end
   end
 
-#  describe "calculating a release for one result (easy)" do
-#    before(:each) do
-#      @hmm_result_nrdb = FactoryGirl.create(:hmm_result_nrdb)
-#      @sequence_source = @hmm_result_nrdb.sequence_source
-#      @pfitmap_release = FactoryGirl.create(:pfitmap_release, sequence_source: @sequence_source)
-#      parse_hmm_tblout(@hmm_result_nrdb, fixture_file_upload("/NrdB-20rows.tblout"))
-#      @sequence_source.evaluate(@pfitmap_release, nil)
-#    end
-#
-#    it "should have a single hmm result registered" do
-#      @pfitmap_release.sequence_source.hmm_results.length.should == 1
-#    end
-#
-#    it "should be successful to call calculate_main" do
-#      @pfitmap_release.calculate_main("GOLDWGStest10", FactoryGirl.create(:user_admin))
+  describe "calculating a release for one result (easy)" do
+    before(:each) do
+      @hmm_result_nrdb = FactoryGirl.create(:hmm_result_nrdb)
+      @sequence_source = @hmm_result_nrdb.sequence_source
+      @pfitmap_release = FactoryGirl.create(:pfitmap_release, sequence_source: @sequence_source)
+      parse_hmm_tblout(@hmm_result_nrdb, fixture_file_upload("/NrdB-20rows.tblout"))
+      @sequence_source.evaluate(@pfitmap_release, nil)
+      @sd = SequenceDatabase.create(db: "ref")
+      @ld = @sd.load_databases.create(taxonset: "http://biosql.scilifelab.se/organism_group_name2full_taxon_hierarchies/GOLDWGStest10.json", active: true)
+    end
+
+    it "should have a single hmm result registered" do
+      @pfitmap_release.sequence_source.hmm_results.length.should == 1
+    end
+
+    it "should be successful to call calculate_released_db" do
+      @pfitmap_release.calculate_released_dbs(@ld)
 #      # warn "#{__FILE__}:#{__LINE__}: ProteinCount.all:\n\t#{ProteinCount.all.map { |pc| "#{pc}" }.join("\n\t")}"
-#      taxons = Taxon.all
-##TODO I changed Taxon.all.length.should from 51 to 10, I haven't verified that this is correct number, need more tests
-##TODO I changed Protein.all.length.should from 2 to 1, I haven't verified that this is correct number, need more tests
-##TODO I changed ProteinCount.all.length.should from 102 to 10, I haven't verified that this is correct number, need more tests
-#      Taxon.all.length.should == 10
+      @rd = ReleasedDb.find(:first, conditions: {load_database_id: @ld, pfitmap_release_id: @pfitmap_release})
+      taxons = Taxon.where(released_db_id: @rd)
+      proteins = Protein.where(released_db_id: @rd)
+      protein_counts = ProteinCount.where(released_db_id: @rd)
+#TODO Verify thesee numbers, only put in since these are the values that came up      
+      taxons.length.should == 10
 #      Enzyme.all.length.should == 1
-#      Protein.all.length.should == 1
-#      ProteinCount.all.length.should == 10
-##      ProteinCount.sum("no_proteins").should == 86
-##      ProteinCount.maximum("no_proteins").should == 5
-##      ProteinCount.maximum("no_genomes_with_proteins").should == 4
-#    end
+      proteins.length.should == 1
+      protein_counts.length.should == 4 
+      protein_counts.sum("no_proteins").should == 5 
+      protein_counts.maximum("no_proteins").should == 2 
+      protein_counts.maximum("no_genomes_with_proteins").should == 1
+   end
 #
-#    it "should not include all taxon-levels" do
-#      @pfitmap_release.calculate_main("GOLDWGStest10", FactoryGirl.create(:user_admin))
-#      Taxon.all.length.should be < 70
-#      
-##TODO Reomved this since no hierarchy in taxons
-##      root_taxon = Taxon.find_by_name("root")
-##      root_taxon.should_not be_nil
-##      # no loose branches in the tree of life:
-##      root_taxons = Taxon.find_all_by_parent_ncbi_id(nil)
-##      root_taxons.length.should == 1
-##      root_taxons.first.hierarchy.should == "root"
-##      root_taxons.first.children.order('hierarchy').first.hierarchy.should == "root:Bacteria"
-##      Taxon.find_all_by_hierarchy(nil).should == []
-##      Taxon.find_all_by_name("Metazoa").should_not == nil
-#    end
-#    it "should name the taxa levels correctly" do
-#      @pfitmap_release.calculate_main("GOLDWGStest10", FactoryGirl.create(:user_admin))
-#      t1 = Taxon.find(:first, conditions: {"strain" => "Acaryochloris marina MBIC11017"})
-#      t1.domain.should == "Bacteria"
-#      t1.kingdom.should == "Bacteria, no kingdom" 
-#      t1.phylum.should ==  "Cyanobacteria"
-#      t1.taxclass.should == "Cyanobacteria, no class"
-#      t1.taxorder.should == "Chroococcales"
-#      t1.family.should ==  "Chroococcales, no family"
-#      t1.genus.should == "Acaryochloris"
-#      t1.species.should == "Acaryochloris marina"
-#      t1.strain.should == "Acaryochloris marina MBIC11017"
-#      t2 = Taxon.find(:first, conditions: {"species" => "Homo sapiens"})
-#      t2.domain.should == "Eukaryota"
-#      t2.species.should == "Homo sapiens"
-#      t2.strain.should == nil
-#    end
-#  end
+    it "should not include all taxon-levels" do
+      @pfitmap_release.calculate_released_dbs(@ld)
+      @rd = ReleasedDb.find(:first, conditions: {load_database_id: @ld, pfitmap_release_id: @pfitmap_release})
+      taxons = Taxon.where(released_db_id: @rd)
+      taxons.length.should be == 10 
+    end
+
+    it "should name the taxa levels correctly" do
+      @pfitmap_release.calculate_released_dbs(@ld)
+      t1 = Taxon.find(:first, conditions: {"strain" => "Acaryochloris marina MBIC11017"})
+      t1.domain.should == "Bacteria"
+      t1.kingdom.should == "Bacteria, no kingdom" 
+      t1.phylum.should ==  "Cyanobacteria"
+      t1.taxclass.should == "Cyanobacteria, no class"
+      t1.taxorder.should == "Chroococcales"
+      t1.family.should ==  "Chroococcales, no family"
+      t1.genus.should == "Acaryochloris"
+      t1.species.should == "Acaryochloris marina"
+      t1.strain.should == "Acaryochloris marina MBIC11017"
+      t2 = Taxon.find(:first, conditions: {"species" => "Homo sapiens"})
+      t2.domain.should == "Eukaryota"
+      t2.species.should == "Homo sapiens"
+      t2.strain.should == nil
+    end
+  end
 #
 #  describe "calculating a release for two results (medium)" do
 #    before(:each) do
